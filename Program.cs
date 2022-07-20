53301035f5fdf59582fa508f1753072f342be6dc
Ihor Volokhovych <igorvolokhovych@gmail.com>
2022-07-20 12:32:06 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Bogus;
+using NetJsonOutOfMemory;
+
+Faker<TestModel> TestModelFaker = new();
+
+var testModels = TestModelFaker
+    .RuleFor(testModel => testModel.TestByte, fakerSetter => fakerSetter.Random.Byte())
+    .RuleFor(testModel => testModel.TestChar, fakerSetter => fakerSetter.Random.Char('a', 'z'))
+    .RuleFor(testModel => testModel.TestDate, fakerSetter => fakerSetter.Date.Past().ToUniversalTime())
+    .RuleFor(testModel => testModel.TestDouble, fakerSetter => fakerSetter.Random.Double())
+    .RuleFor(testModel => testModel.TestFloat, fakerSetter => fakerSetter.Random.Float())
+    .RuleFor(testModel => testModel.TestInt, fakerSetter => fakerSetter.Random.Int())
+    .RuleFor(testModel => testModel.TestLong, fakerSetter => fakerSetter.Random.Long())
+    .RuleFor(testModel => testModel.TestShort, fakerSetter => fakerSetter.Random.Short())
+    .RuleFor(testModel => testModel.TestString, fakerSetter => fakerSetter.Random.String2(5, 10))
+    .RuleFor(testModel => testModel.TestUInt, fakerSetter => fakerSetter.Random.UInt())
+    .RuleFor(testModel => testModel.TestUShort, fakerSetter => fakerSetter.Random.UShort())
+    .RuleFor(testModel => testModel.TestULong, fakerSetter => fakerSetter.Random.ULong())
+    .RuleFor(testModel => testModel.TestTimeSpan, fakerSetter => fakerSetter.Date.Timespan())
+    // .RuleFor(testModel => testModel.TestByteArray, fakerSetter => fakerSetter.Random.Bytes(internalCount))
+    .RuleFor(testModel => testModel.TestCharArray, fakerSetter => fakerSetter.Random.Chars('a', 'z', count: 10))
+    .RuleFor(testModel => testModel.TestDoubleArray, fakerSetter => fakerSetter.GetArray(func => func.Random.Double(), 10))
+    .RuleFor(testModel => testModel.TestFloatArray, fakerSetter => fakerSetter.GetArray(func => func.Random.Float(), 10))
+    .RuleFor(testModel => testModel.TestIntArray, fakerSetter => fakerSetter.GetArray(func => func.Random.Int(), 10))
+    .RuleFor(testModel => testModel.TestUIntArray, fakerSetter => fakerSetter.GetArray(func => func.Random.UInt(), 10))
+    .RuleFor(testModel => testModel.TestLongArray, fakerSetter => fakerSetter.GetArray(func => func.Random.Long(), 10))
+    .RuleFor(testModel => testModel.TestShortArray, fakerSetter => fakerSetter.GetArray(func => func.Random.Short(), 10))
+    .RuleFor(testModel => testModel.TestStringArray, fakerSetter => fakerSetter.GetArray(func => func.Random.String2(5, 10), 10))
+    .RuleFor(testModel => testModel.TestUShortArray, fakerSetter => fakerSetter.GetArray(func => func.Random.UShort(), 10))
+    .RuleFor(testModel => testModel.TestULongArray, fakerSetter => fakerSetter.GetArray(func => func.Random.ULong(), 10))
+    .Generate(10)
+    .ToArray();
+    
+NetJSON.NetJSONSettings netJsonOptions = new()
+{
+    DateFormat = NetJSON.NetJSONDateFormat.ISO,
+    TimeZoneFormat = NetJSON.NetJSONTimeZoneFormat.Utc,
+    CaseSensitive = false
+};
+
+var actualString =  NetJSON.NetJSON.Serialize(testModels, netJsonOptions);
+//todo: cope with System.OutOfMemoryException: Array dimensions exceeded supported range.
+var actualModels = NetJSON.NetJSON.Deserialize<TestModel[]>(actualString, netJsonOptions);
+
+Console.ReadKey();
---
+++ TestModel.cs
@@ -0,0 +1,147 @@
+﻿namespace NetJsonOutOfMemory;
+
+using System.Runtime.Serialization;
+
+/// <summary>
+///     Test model.
+/// </summary>
+public class TestModel
+{
+    /// <summary>
+    ///     Gets or sets test string value.
+    /// </summary>
+    [DataMember(Name = "testString")]
+    public virtual string TestString { get; set; } = string.Empty;
+
+    /// <summary>
+    ///     Gets or sets test string array values.
+    /// </summary>
+    [DataMember(Name = "testStringArray")]
+    public virtual string[] TestStringArray { get; set; }  = Array.Empty<string>();
+    
+    /// <summary>
+    ///     Gets or sets test int value.
+    /// </summary>
+    [DataMember(Name = "testInt")]
+    public virtual int TestInt { get; set; }
+    
+    /// <summary>
+    ///     Gets or sets test int array values.
+    /// </summary>
+    [DataMember(Name = "testIntArray")]
+    public virtual int[] TestIntArray { get; set; } = Array.Empty<int>();
+
+    /// <summary>
+    ///     Gets or sets test double value.
+    /// </summary>
+    [DataMember(Name = "testDouble")]
+    public virtual double TestDouble { get; set; }
+    
+    /// <summary>
+    ///     Gets or sets test double array values.
+    /// </summary>
+    [DataMember(Name = "testDoubleArray")]
+    public virtual double[] TestDoubleArray { get; set; } = Array.Empty<double>();
+    
+    /// <summary>
+    ///     Gets or sets test float value.
+    /// </summary>
+    [DataMember(Name = "testFloat")]
+    public virtual float TestFloat { get; set; }
+    
+    /// <summary>
+    ///     Gets or sets test float array values.
+    /// </summary>
+    [DataMember(Name = "testFloatArray")]
+    public virtual float[] TestFloatArray { get; set; } = Array.Empty<float>();
+    
+    /// <summary>
+    ///     Gets or sets test uint value.
+    /// </summary>
+    [DataMember(Name = "testUInt")]
+    public virtual uint TestUInt { get; set; }
+    
+    /// <summary>
+    ///     Gets or sets test uint array values.
+    /// </summary>
+    [DataMember(Name = "testUIntArray")]
+    public virtual uint[] TestUIntArray { get; set; } = Array.Empty<uint>();
+    
+    /// <summary>
+    ///     Gets or sets test char value.
+    /// </summary>
+    [DataMember(Name = "testChar")]
+    public virtual char TestChar { get; set; }
+    
+    /// <summary>
+    ///     Gets or sets test char array values.
+    /// </summary>
+    [DataMember(Name = "testCharArray")]
+    public virtual char[] TestCharArray { get; set; } = Array.Empty<char>();
+    
+    /// <summary>
+    ///     Gets or sets test byte value.
+    /// </summary>
+    [DataMember(Name = "testByte")]
+    public virtual byte TestByte { get; set; }
+
+    /// <summary>
+    ///     Gets or sets test short value.
+    /// </summary>
+    [DataMember(Name = "testShort")]
+    public virtual short TestShort { get; set; }
+    
+    /// <summary>
+    ///     Gets or sets test short array values.
+    /// </summary>
+    [DataMember(Name = "testShortArray")]
+    public virtual short[] TestShortArray { get; set; } = Array.Empty<short>();
+    
+    /// <summary>
+    ///     Gets or sets test ushort value.
+    /// </summary>
+    [DataMember(Name = "testUShort")]
+    public virtual ushort TestUShort { get; set; }
+    
+    /// <summary>
+    ///     Gets or sets test ushort array values.
+    /// </summary>
+    [DataMember(Name = "testUShortArray")]
+    public virtual ushort[] TestUShortArray { get; set; } = Array.Empty<ushort>();
+    
+    /// <summary>
+    ///     Gets or sets test long value.
+    /// </summary>
+    [DataMember(Name = "testLong")]
+    public virtual long TestLong { get; set; }
+
+    /// <summary>
+    ///     Gets or sets test long array values.
+    /// </summary>
+    [DataMember(Name = "testLongArray")]
+    public virtual long[] TestLongArray { get; set; } = Array.Empty<long>();
+    
+    /// <summary>
+    ///     Gets or sets test ulong value.
+    /// </summary>
+    [DataMember(Name = "testULong")]
+    public virtual ulong TestULong { get; set; }
+    
+    /// <summary>
+    ///     Gets or sets test ulong array values.
+    /// </summary>
+    [DataMember(Name = "testULongArray")]
+    public virtual ulong[] TestULongArray { get; set; } = Array.Empty<ulong>();
+    
+    /// <summary>
+    ///     Gets or sets test DateTime value.
+    /// </summary>
+    [DataMember(Name = "testDate")]
+    public virtual DateTime TestDate { get; set; } = DateTime.UtcNow;
+    
+    /// <summary>
+    ///     Gets or sets test TimeSpan value.
+    /// </summary>
+    [DataMember(Name = "testTimeSpan")]
+    public virtual TimeSpan TestTimeSpan { get; set; } = TimeSpan.Zero;
+}
